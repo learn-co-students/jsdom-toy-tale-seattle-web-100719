df59f64b9992125d591ef1565d252e760e49fa0e
Hallie Dunn <hal.dunn@flatironschool.com>
2019-11-21 14:21:57 -0800

    ?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-let addToy = false
+  let addToy = false
+  let url = "http://localhost:3000/toys"
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  const otherForm = document.querySelector('.add-toy-form')
+  let toyDiv = document.getElementById("toy-collection")
+  
+  otherForm.addEventListener("submit", () => {addNewToy(event)})
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +18,85 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
+  fetch(url)
+  .then(res=>res.json())
+  .then(json=>{
+    createToys(json)
+  })
+
+  function createToys(json){
+    for(let i=0; i<json.length; i++){
+      appendToyCard(json[i])
+    }
+  }
+
+  function likeToy(toy){
+    toy.likes++
+    let patchData = JSON.stringify(toy)
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: patchData
+    })
+
+    let likesOnPage = document.getElementById(toy.id)
+    likesOnPage.innerText = `${parseInt(likesOnPage.innerText) + 1} request(s) for death` 
+  }
+
+  function addNewToy(e){
+    e.preventDefault()
+    let inputs = document.getElementsByClassName("input-text")
+    inputs[0].value = ''
+    inputs[1].value = ''
+    console.log(inputs[0].value, "<--That is the value of the input")
+    let postData = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(res=>res.json())
+    .then(json=>{
+      if(json.id){
+        appendToyCard(json)
+      }
+      else {
+        alert("something fucked up")
+      }
+    })
+  }
+
+  function appendToyCard(toyInfo){
+    let card = document.createElement('div')
+    card.className = "card"
+    let h2 = document.createElement('h2')
+    h2.innerText = toyInfo.name
+    let img = document.createElement('img')
+    img.className = "toy-avatar"
+    img.src = toyInfo.image
+    let p = document.createElement('p')
+    p.innerText = `${toyInfo.likes} request(s) for death`
+    p.id = toyInfo.id
+    let btn = document.createElement('button')
+    btn.className = 'like-btn'
+    btn.innerText = "let me die"
+    btn.addEventListener('click', () => likeToy(toyInfo))
+
+    card.appendChild(h2)
+    card.appendChild(img)
+    card.appendChild(p)
+    card.appendChild(btn)
+    toyDiv.appendChild(card)
+  }
 
 })
